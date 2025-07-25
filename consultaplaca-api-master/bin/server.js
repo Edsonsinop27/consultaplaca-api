9530fbc9a68747acdeb3d9847cbdcdf53fb2d5da
Edsonsinop27 <edsonsinop27@gmail.com>
2025-07-25 18:11:21 -0400

    Delete consultaplaca-api-master directory

--- consultaplaca-api-master/bin/server.js
+++
@@ -1,7 +0,0 @@
-const logger = require('simple-node-logger').createSimpleLogger();
-
-const app = require('../src/app');
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => logger.info(`API listening on port => ${port}`));
--- consultaplaca-api-master/src/routes/plate-route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const controller = require('../controllers/plate-controller');
-
-router.get('/:plate', controller.get);
-
-module.exports = router;
