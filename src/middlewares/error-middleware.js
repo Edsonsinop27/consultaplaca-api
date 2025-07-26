2086039e58e761e78c088a278a49a3d57e914e40
Edsonsinop27 <edsonsinop27@gmail.com>
2025-07-26 19:31:14 -0400

    Update error-middleware.js

--- src/middlewares/error-middleware.js
+++ src/middlewares/error-middleware.js
@@ -1,13 +1,11 @@
+// src/middlewares/error-middleware.js
 function notFoundError(req, res) {
   res.status(404).json({ error: 'Rota não encontrada.' });
 }
 
-// 500 Internal Server Error
 function internalServerError(err, req, res, next) {
-  console.error('Internal error:', err.stack || err);
-  res.status(500).json({
-    error: 'Erro interno ao processar a requisição.'
-  });
+  console.error('Erro interno:', err.stack || err);
+  res.status(500).json({ error: 'Erro interno ao processar requisição.' });
 }
 
 module.exports = { notFoundError, internalServerError };
