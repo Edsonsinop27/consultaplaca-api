d33e456b43d2a98cdff7eb1743289b928cc3a62c
Edsonsinop27 <edsonsinop27@gmail.com>
2025-07-26 19:30:19 -0400

    Update placa-route.js

--- src/routes/placa-route.js
+++ src/routes/placa-route.js
@@ -1,10 +1,10 @@
 // src/routes/placa-route.js
-const express       = require('express');
-const { getPlaca }  = require('../controllers/placa-controller');
+const express      = require('express');
+const { getPlaca } = require('../controllers/placa-controller');
 
 const router = express.Router();
 
-// roda quando chamar GET /placa/:placa
-router.get('/:placa', getPlaca);
+// GET /placa/:placa
+router.get('/placa/:placa', getPlaca);
 
 module.exports = router;
